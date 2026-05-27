02f7d417be6ff49e62c8aa3a12f2cea84203a445
Your Name <you@example.com>
2026-05-26 21:48:45 -0300

    continuacao

--- src/main/java/com/clinica/veterinaria/repository/AnimalRepository.java
+++ src/main/java/com/clinica/veterinaria/repository/AnimalRepository.java
@@ -1,7 +1,10 @@
 package com.clinica.veterinaria.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.clinica.veterinaria.model.Animal;
 
 public interface AnimalRepository extends JpaRepository<Animal, Long>{
     
+    List<Animal> findByNomeContainingIgnoreCase(String nome);
 }
